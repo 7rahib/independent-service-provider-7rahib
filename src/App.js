34bfb2307aebcd2f046b6c8166ff03b9f9265085
Naimur Rahib <61423411+7rahib@users.noreply.github.com>
2022-04-17 01:03:20 +0600

    react router initialised

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import './App.css';
 import Header from './Pages/Shared/Header/Header';
 import Home from './Pages/Home/Home/Home';
 import Services from './Pages/Home/Services/Services';
+import { Route, Routes } from 'react-router-dom';
 import Footer from './Pages/Shared/Footer/Footer';
+import Login from './Pages/Registration/Login/Login';
+import About from './Pages/About/About';
+import Blogs from './Pages/Blogs/Blogs';
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Home></Home>
-      <Services></Services>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/services" element={<Services></Services>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/blogs" element={<Blogs></Blogs>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+      </Routes>
       <Footer></Footer>
     </div>
   );
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,29 +1,30 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
+        <>
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
                 <Container>
-                    <Navbar.Brand href="#home">Story Maker</Navbar.Brand>
+                    <Navbar.Brand as={Link} to="home">Story Maker</Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="#features">Services</Nav.Link>
-                            <Nav.Link href="#pricing">Blogs</Nav.Link>
-                            <Nav.Link href="#pricing">About</Nav.Link>
+                            <Nav.Link as={Link} to="services">Services</Nav.Link>
+                            <Nav.Link as={Link} to="blogs">Blogs</Nav.Link>
+                            <Nav.Link as={Link} to="about">About</Nav.Link>
                         </Nav>
                         <Nav>
-                            <Nav.Link href="#deets">User Name</Nav.Link>
-                            <Nav.Link eventKey={2} href="#memes">
+                            <Nav.Link to="deets">User Name</Nav.Link>
+                            <Nav.Link eventKey={2} as={Link} to="login">
                                 Login
                             </Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
-        </div>
+        </>
     );
 };
 
